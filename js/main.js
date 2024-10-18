bdedce6dc955e357fd694834839924e66ec6011d
Adrian Martin <martinsebitasrodriguezcortez@gmail.com>
2024-10-18 08:51:25 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+document.write("hola mundo");
+/*  For es un ciclo en eel cual hay 3 parametros la funcion es repetir un bloque de codigo las veces necesarias */
+/* sintaxis basica 
+for (condicion){
+bloque de codigo
+
+}
+*/
+/*for(i=0; i<=10; i++){
+
+    //document.write("hola desde js" + "<br>");
+    document.write(i+"<br>")
+}
+/*for(i=0; i<=10; i=i+2){
+}*/
+//i+=2
+//numeros pares del 3 al 15
+/*for(i=3; i<=15; i%0){
+    document.write(i+"<br>")
+   
+}*/
+if(i%2==0){
+document.write("numeros par :" +i +"<br>")
+
+}
+/*ejercciio que calcule el factorial de un numero ingresado por teclado*/
+//enviar en la 4.3.1 
+//ponte ingresa 7
+// seria 1*2*3*4*5*6*7= 5040
